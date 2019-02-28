061376287e4d949e7a6ad6bb9898a44b244f31fc
Phillip Lakis <p.lakis@icloud.com>
2019-02-28 16:39:30 +1100

    Update fastai.sh

--- fastai.sh
+++ fastai.sh
@@ -3,35 +3,12 @@
 #  COURSE v3
 #  FastAI 1.0
 #  Please do note that running scripts like this from the Internet is a dangerous practice
+sudo service lightdm stop
 
-echo -e "Installation script for Ubuntu 16+"
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/418.43/NVIDIA-Linux-x86_64-418.43.run
+bash NVIDIA-Linux-x86_64-418.43.run
 
-echo -e "Set non-interactive frontend"
-echo -e "Script will run without any prompts"
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y \
-    apt-utils \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    wget \
-    software-properties-common
-
-echo -e "\n###\n"
-echo -e "Installing NVIDIA drivers and CUDA"
-echo -e "Driver version: 410.79"
-echo -e "CUDA version: 10.0.130-1"
-echo -e "\n###\n"
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-
-dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-
-apt-get update
-apt-get install cuda -y
+sudo service lightdm start
 
 echo -e "\n###\n"
 echo -e "Installing Anaconda3"
@@ -40,15 +17,28 @@ echo -e "\n###\n"
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh -b
 
-echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
-export PATH=~/anaconda3/bin:$PATH
-source ~/.bashrc
-
 echo -e "\n###\n"
 echo -e "Installing FastAI v3"
 echo -e "\n###\n"
 
-conda install -c pytorch -c fastai fastai
+cd
+git clone https://github.com/fastai/fastai.git
+cd fastai/
+
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
+source ~/.bashrc
+cd ..
+
+conda create -n fastai python=3.6
+source activate fastai
+conda install pytorch torchvision -c pytorch 
+conda install -c fastai fastai 
+conda env update
+
+echo 'source activate fastai' >> ~/.bashrc
+source activate fastai
+source ~/.bashrc
 
 echo -e "\n###\n"
 echo -e "Downloading Dogs Cats Dataset"
